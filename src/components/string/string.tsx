fadf1118740c9ea01fe0a7cb9d7931f6e11c968d
Maxim Chalikov <aracctur@yandex.ru>
2023-07-21 21:17:38 +0300

    feat: сделать начальное отображение строки до развертывания

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -3,12 +3,19 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import styles from './string.module.css'
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
 
 export const StringComponent: React.FC = () => {
    const [line, setLine] = useState('');
+   const [arr, setArr] = useState<string[]>([])
 
    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       setLine(e.target.value)
+   }
+
+   const onClick = () => {
+      setArr(line.split(''))
    }
 
    return (
@@ -28,8 +35,20 @@ export const StringComponent: React.FC = () => {
                   type="button"
                   isLoader={false}
                   linkedList={"small"}
-                  disabled={line ? true : false}
+                  disabled={line ? false : true}
+                  onClick={onClick}
                />
+            </div>
+            <div className={styles.letter_box}>
+               {
+                  arr.map((letter, index) => (
+                     <Circle
+                        key={index}
+                        state={ElementStates.Default}
+                        letter={letter}
+                     />
+                  ))
+               }
             </div>
          </div>
       </SolutionLayout>
