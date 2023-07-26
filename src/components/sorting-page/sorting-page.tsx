46d5b8c7a103f26764471b723ea4f81cf93be68e
Maxim Chalikov <aracctur@yandex.ru>
2023-07-26 15:47:46 +0300

    feat: настроить переключение радиокнопок

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { RadioInput } from '../ui/radio-input/radio-input';
 import styles from './sorting-page.module.css'
@@ -14,28 +14,37 @@ interface IColumn {
    state: ElementStates
 }
 
+enum AlgorithmMethod { SelectionSort, BubbleSort }
+
 export const SortingPage: React.FC = () => {
    const minLength: number = 3;
    const maxLength = 17;
-   const [arrayNumbers, setArrayNumbers] = useState<IColumn[]>([])
-   const createArray = () => {
+   const [array, setArray] = useState<IColumn[]>([])
+   const [method, setMethod] = useState(AlgorithmMethod.SelectionSort)
+   const createArray = useMemo(() => () => {
       const newArray = randomArr(minLength, maxLength).map((number) => { return { number, state: ElementStates.Default } });
-      setArrayNumbers(newArray)
-   }
+      setArray(newArray)
+   }, [setArray])
+
    useEffect(() => {
       createArray()
    }, []);
 
-   console.log(arrayNumbers)
    return (
       <SolutionLayout title='Сортировка массива'>
          <div className={styles.container}>
             <div className={styles.control_box}>
                <div className={styles.radio_box}>
                   <RadioInput
-                     label='Выбор' />
+                     checked={method === AlgorithmMethod.SelectionSort}
+                     onChange={() => setMethod(AlgorithmMethod.SelectionSort)}
+                     label='Выбор'
+                  />
                   <RadioInput
-                     label='Пузырёк' />
+                     checked={method === AlgorithmMethod.BubbleSort}
+                     onChange={() => setMethod(AlgorithmMethod.BubbleSort)}
+                     label='Пузырёк'
+                  />
                </div>
                <div className={styles.buttons}>
                   <div className={styles.buttons_sorting}>
@@ -63,11 +72,10 @@ export const SortingPage: React.FC = () => {
             </div>
             <div className={styles.array_box}>
                {
-                  arrayNumbers.map(({ number, state }, index) => (
+                  array.map(({ number, state }, index) => (
                      <Column key={index} index={number} state={state} />
                   ))
                }
-
             </div>
          </div>
       </SolutionLayout>
