8d36d41ec819cc708d29cb4d3f903841db9fcda8
Maxim Chalikov <aracctur@yandex.ru>
2023-07-26 13:11:53 +0300

    feat: сделать верстку панели с кнопками

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,45 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React from 'react';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import { RadioInput } from '../ui/radio-input/radio-input';
+import styles from './sorting-page.module.css'
+import { Button } from '../ui/button/button';
+import { Direction } from '../../types/direction';
 
 export const SortingPage: React.FC = () => {
-  return (
-    <SolutionLayout title="Сортировка массива">
-
-    </SolutionLayout>
-  );
+   return (
+      <SolutionLayout title='Сортировка массива'>
+         <div className={styles.container}>
+            <div className={styles.control_box}>
+               <div className={styles.radio_box}>
+                  <RadioInput
+                     label='Выбор' />
+                  <RadioInput
+                     label='Пузырёк' />
+               </div>
+               <div className={styles.buttons}>
+                  <div className={styles.buttons_sorting}>
+                     <Button
+                        text='По возрастанию'
+                        type='button'
+                        sorting={Direction.Ascending}
+                        style={{ minWidth: '205px' }}
+                     />
+                     <Button
+                        text='По убыванию'
+                        type='button'
+                        sorting={Direction.Descending}
+                        style={{ minWidth: '186px' }}
+                     />
+                  </div>
+                  <Button
+                     extraClass={styles.button_reset}
+                     text='Новый массив'
+                     type='reset'
+                     style={{ minWidth: '168px' }}
+                  />
+               </div>
+            </div>
+         </div>
+      </SolutionLayout>
+   );
 };
