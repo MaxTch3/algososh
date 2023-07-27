63b482ca799581e7d82c5c7618bf88139bcb62a1
Maxim Chalikov <aracctur@yandex.ru>
2023-07-27 07:30:24 +0300

    feat: закончить с алгоритмами сортировки, кроме состояния кнопок

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -70,7 +70,6 @@ export const SortingPage: React.FC = () => {
 
    const bubbleSort = async (arr: IColumn[], order: 'asc' | 'desc') => {
       for (let i = 0; i < arr.length; i++) {
-
          for (let j = 0; j < arr.length - i - 1; j++) {
             arr[j].state = ElementStates.Changing;
             arr[j + 1].state = ElementStates.Changing;
