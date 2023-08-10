87f721b5233ceae6852b2a920994d0bc0016f9aa
Maxim Chalikov <aracctur@yandex.ru>
2023-08-10 21:19:49 +0300

    feat: добавить функцию добавления в head и ее анимацию, состояние кнопок в зависимости от заполненности полей

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { Input } from '../ui/input/input';
 import { Button } from '../ui/button/button';
@@ -7,12 +7,50 @@ import { Circle } from '../ui/circle/circle';
 import { List } from './utils';
 import { startingArray } from './constants';
 import { ArrowIcon } from '../ui/icons/arrow-icon';
+import { delay } from '../../utils/utils';
+import { DELAY_IN_MS } from '../../constants/delays';
+import { ElementStates } from '../../types/element-states';
 
 export const ListPage: React.FC = () => {
    const [arrString, setArrString] = useState<string[]>(startingArray);
+   const [inputText, setInputText] = useState('');
+   const [inputIndex, setInputIndex] = useState('');
+   const [insertIndex, setInsertIndex] = useState<number>(NaN);
+   const [modifiedIndex, setModifiedIndex] = useState<number>(NaN);
+   const [isLoadingAddHead, setIsLoadingAddHead] = useState(false);
+
    const listString = useMemo(() => {
       return new List<string>(startingArray)
    }, []);
+
+   const addHead = async () => {
+      setIsLoadingAddHead(true);
+      setInsertIndex(0);
+      await delay(DELAY_IN_MS);
+      listString.unshift(inputText);
+      setArrString(listString.toArray());
+      setInsertIndex(NaN);
+      setModifiedIndex(0);
+      await delay(DELAY_IN_MS);
+      setModifiedIndex(NaN);
+      setIsLoadingAddHead(false);
+   };
+
+   const handleChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputText(e.target.value)
+   }
+
+   const handleChangeIndex = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const minIndex = 0;
+      const maxIndex = listString.toArray().length - 1
+      if (
+         +e.target.value >= minIndex &&
+         +e.target.value <= maxIndex
+      ) {
+         setInputIndex(e.target.value)
+      }
+   }
+
    return (
       <SolutionLayout title='Связный список'>
          <div className={styles.container}>
@@ -23,22 +61,30 @@ export const ListPage: React.FC = () => {
                   maxLength={4}
                   isLimitText={true}
                   style={{ maxWidth: '204px' }}
+                  onChange={handleChangeText}
+                  value={inputText}
                />
                <Button
                   text='Добавить&nbsp;в&nbsp;head'
                   style={{ minWidth: '175px' }}
+                  onClick={addHead}
+                  isLoader={isLoadingAddHead}
+                  disabled={!inputText}
                />
                <Button
                   text='Добавить в tail'
                   style={{ minWidth: '175px' }}
+                  disabled={!inputText || isLoadingAddHead}
                />
                <Button
                   text='Удалить&nbsp;из&nbsp;head'
                   style={{ minWidth: '175px' }}
+                  disabled={!arrString || isLoadingAddHead}
                />
                <Button
                   text='Удалить из tail'
                   style={{ minWidth: '175px' }}
+                  disabled={!arrString || isLoadingAddHead}
                />
             </div>
             <div className={styles.control_box}>
@@ -47,24 +93,32 @@ export const ListPage: React.FC = () => {
                   type='text'
                   maxLength={4}
                   style={{ maxWidth: '204px' }}
+                  value={inputIndex}
+                  onChange={handleChangeIndex}
                />
                <Button
                   text='Добавить по индексу'
                   style={{ minWidth: '362px' }}
+                  disabled={!inputIndex || !inputText || isLoadingAddHead}
                />
                <Button
                   text='Удалить по индексу'
                   style={{ minWidth: '362px' }}
+                  disabled={!arrString || !inputIndex || isLoadingAddHead}
                />
             </div>
             <div className={styles.array_box}>
                {arrString &&
                   arrString.map((item, index) => (
-                     <div className={styles.item_box}>
+                     <div className={styles.item_box} key={index}>
                         <Circle
-                           key={index}
+                           head={insertIndex === index
+                              ? <Circle isSmall letter={inputText} state={ElementStates.Changing} />
+                              : index === 0 ? 'head' : ''}
+                           tail={index === arrString.length - 1 ? 'tail' : ''}
                            index={index}
                            letter={item}
+                           state={modifiedIndex === index ? ElementStates.Modified : ElementStates.Default}
                         />
                         {index < arrString.length - 1 &&
                            <ArrowIcon />
