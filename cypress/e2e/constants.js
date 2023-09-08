0d67d4c96f562a396a95980d99b65901521d4880
Maxim Chalikov <aracctur@yandex.ru>
2023-09-08 12:22:29 +0300

    feat: добавить проверку поднятия приложения и проверку роутинга страниц

---
+++ cypress/e2e/constants.js
@@ -0,0 +1,9 @@
+export const baseUrl = 'http://localhost:3000';
+export const extraPageInfo = [
+   { name: 'Строка', url: 'recursion'}, 
+   { name: 'Последовательность Фибоначчи', url: 'fibonacci'},
+   { name: 'Сортировка массива', url: 'sorting'},
+   { name: 'Стек', url: 'stack'},
+   { name: 'Очередь', url: 'queue'},
+   { name: 'Связный список', url: 'list'}
+]
