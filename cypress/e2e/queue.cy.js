44aca63c0b6db303e1e51b179d0567b49c8e85ad
Maxim Chalikov <aracctur@yandex.ru>
2023-09-11 02:57:49 +0300

    feat: написать тест на корректность стилей и значений при добавлени элемента в очередь

--- cypress/e2e/queue.cy.js
+++ cypress/e2e/queue.cy.js
@@ -1,10 +1,13 @@
-import { baseUrl, queueUrl } from "./constants";
+import { SHORT_DELAY_IN_MS } from "../../src/constants/delays";
+import { ElColor, baseUrl, queueUrl } from "./constants";
+
+const textArray = ['А', 'Б', 'В', 'Г', 'Д'];
 
 describe('Тестирование страницы Очередь', () => {
 
    beforeEach(() => {
       cy.visit(baseUrl + queueUrl);
-      cy.clock();
+      cy.clock()
    });
 
    it('Кнопка Добавить заблокированна при пустом поле input', () => {
@@ -14,4 +17,42 @@ describe('Тестирование страницы Очередь', () => {
          }
       });
    });
-})
+
+   it('Корректность стилей и значений при добавлени элемента в очередь', () => {
+
+      for (let i = 0; i < textArray.length; i++) {
+         cy.get('[test-id="textInput"]').type(textArray[i]);
+         cy.get('[test-id="addButton"]').click()
+
+         cy.get('[test-id="circle"]').each((element, index) => {
+            if (i === index) {
+               cy.get(element).should('have.css', 'border-color', ElColor.changing);
+               cy.get(element).should('not.have.text');
+
+            }
+            if (index < i) {
+               cy.get(element).should('have.css', 'border-color', ElColor.default);
+               cy.get(element).should('have.text', textArray[index])
+            }
+            if (index === i - 1 && i > 0) {
+               cy.get(element).parent().get('[test-id="tail"]').should('have.text', 'tail')
+            } else {
+               cy.get(element).parent().get('[test-id="tail"]').should('not.have.text')
+            }
+            if (index === 0 && i > 0) {
+               cy.get(element).parent().get('[test-id="head"]').should('have.text', 'head')
+            } else {
+               cy.get(element).parent().get('[test-id="head"]').should('not.have.text')
+            }
+         })
+
+         cy.tick(SHORT_DELAY_IN_MS);
+
+         cy.get('[test-id="circle"]').eq(i).should('have.css', 'border-color', ElColor.default)
+         cy.get('[test-id="circle"]').eq(i).should('have.text', textArray[i])
+      }
+   });
+
+});
+
+
